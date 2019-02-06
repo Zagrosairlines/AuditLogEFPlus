acb7347804c52af296ba55495355665eb824d9e1
Hadi Hoseini <mir.hadi.peyrov@gmail.com>
2019-02-06 16:29:58 +0330

    add person model

--- AuditLogTest/AuditLogTest/App_Start/RouteConfig.cs
+++ AuditLogTest/AuditLogTest/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AuditLogTest
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "People", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ AuditLogTest/AuditLogTest/Migrations/201902061256154_InitialCreate.cs
@@ -0,0 +1,28 @@
+namespace AuditLogTest.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Age = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.People");
+        }
+    }
+}
